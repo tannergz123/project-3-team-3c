74b14891d0cdfb723cfe43cfce49fec89174b82e
TannerG <tannergaoz@gmail.com>
2024-10-28 13:18:56 -0500

    removed some stuff

---
+++ client/app/page.tsx
@@ -0,0 +1,11 @@
+import {
+  Box,
+} from "@chakra-ui/react"
+
+export default async function Page() {
+  return (
+    <Box textAlign="center" fontSize="xl" pt="30vh">
+      <h1>Hello World</h1>
+    </Box>
+  )
+}
