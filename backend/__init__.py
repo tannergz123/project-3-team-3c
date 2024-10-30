031fd213ed4e8b9a4f408e22e5bce167a9adb547
apend10 <apendela10@gmail.com>
2024-10-30 10:31:06 -0500

    backend works locally

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,20 +1,11 @@
 from flask import Flask, request, jsonify
 
-from backend.views import employees
-# from .views import ingredients_order_history
-# from .views import ingredients
-# from backend.views import items_ingredients
-# from backend.views import items_order_history
-# from backend.views import items
-# from backend.views import menu_item_prices
-# from backend.views import order_items
-# from backend.views import order_sub_items
-# from backend.views import orders
+from .views.employees import employees
 
 def create_app():
     app = Flask(__name__)
 
     #register blueprints
-    app.register_blueprint(employees)
+    app.register_blueprint(employees, url_prefix = '/employees')
 
     return app
