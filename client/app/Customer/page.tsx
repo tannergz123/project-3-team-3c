562583b0977879d19374cdcadf4e2842c7d20e0d
Iridium01 <dongkyd679@gmail.com>
2024-10-30 16:28:02 -0500

    dummy customer page

---
+++ client/app/Customer/page.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import {
+    Box,
+  } from "@chakra-ui/react"
+
+export default async function Page() {
+    return (
+        <Box textAlign="center" fontSize="xl" pt="10vh">
+            <Box display="flex" justifyContent="center" pt="5vh" pb="2vh">
+                <p>Customer Page</p>
+            </Box>
+        </Box>
+    )
+}
