d29afed57c4ef8ef52483f4ff12209b38b29741b
Iridium01 <dongkyd679@gmail.com>
2024-11-03 18:51:09 -0600

    add images to menuboard

--- client/app/MenuBoard/page.tsx
+++ client/app/MenuBoard/page.tsx
@@ -15,7 +15,7 @@ import {
 // Types for menu items
 type MenuItem = {
   name: string;
-  image: string; // URL to image
+  image: string; // URL to image or StaticImageData
 };
 
 type ComboOption = {
@@ -26,26 +26,42 @@ type ComboOption = {
 
 // Combo options data
 const combos: ComboOption[] = [
-  { name: "1-Entrée Combo", price: 7.99, details: "Includes 1 entrée and 1 side" },
-  { name: "2-Entrée Combo", price: 9.99, details: "Includes 2 entrées and 1 side" },
-  { name: "3-Entrée Combo", price: 11.99, details: "Includes 3 entrées and 1 side" }
+  { name: "Bowl", price: 7.99, details: "Includes 1 entrée and 1 side" },
+  { name: "Plate", price: 9.99, details: "Includes 2 entrées and 1 side" },
+  { name: "Bigger Plate", price: 11.99, details: "Includes 3 entrées and 1 side" }
 ];
 
 // Sides data
 const sides: MenuItem[] = [
-  { name: "Chow Mein", image: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Sides_ChowMein.png" },
-  { name: "Fried Rice", image: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Sides_FriedRice.png" },
-  { name: "White Steamed Rice", image: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Sides_WhiteSteamedRice.png" },
-  { name: "Super Greens", image: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Vegetables_SuperGreens.png" }
+  { name: "Chow Mein", image: "/menu_images/side/chow_mein.png" },
+  { name: "Fried Rice", image: "/menu_images/side/fried_rice.png" },
+  { name: "White Steamed Rice", image: "/menu_images/side/white_rice.png" },
+  { name: "Super Greens", image: "/menu_images/side/super_greens.png" }
 ];
 
 // Entrees data
 const entrees: MenuItem[] = [
-  { name: "Orange Chicken", image: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Chicken_OrangeChicken.png" },
-  { name: "Beijing Beef", image: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Beef_BeijingBeef.png" },
-  { name: "Broccoli Beef", image: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Beef_BroccoliBeef.png" },
-  { name: "Honey Walnut Shrimp", image: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Seafood_HoneyWalnutShrimp.png" }
+  { name: "Orange Chicken", image: "/menu_images/entree/orange_chicken.png" },
+  { name: "Beijing Beef", image: "/menu_images/entree/beijing_beef.png" },
+  { name: "Broccoli Beef", image: "/menu_images/entree/broccoli_beef.png" },
+  { name: "Honey Walnut Shrimp", image: "/menu_images/entree/hw_shrimp.png" },
+  { name: "Honey Sesame Chicken Breast", image: "/menu_images/entree/honey_sesame.png" },
+  { name: "Mushroom Chicken", image: "/menu_images/entree/mushroom_chicken.png" },
+  { name: "SweetFire Chicken Breast", image: "/menu_images/entree/sf_chicken.png" },
+  { name: "Teriyaki Chicken", image: "/menu_images/entree/teriyaki_chicken.png" },
+  { name: "Kung Pao Chicken", image: "/menu_images/entree/kp_chicken.png" },
+  { name: "Black Pepper Sirloin Steak", image: "/menu_images/entree/bp_steak.png" },
+  { name: "Black Pepper Chicken", image: "/menu_images/entree/bp_chicken.png" },
+  { name: "String Bean Chicken Breast", image: "/menu_images/entree/sb_chicken.png" }
 ];
+
+// Appetizers data
+const appetizers: MenuItem[] = [
+  { name: "Chicken Egg Roll", image: "/menu_images/appetizer/chicken_egg_roll.avif" },
+  { name: "Cream Cheese Rangoon", image: "/menu_images/appetizer/cream_cheese_rangoon.avif" },
+  { name: "Apple Pie Roll", image: "/menu_images/appetizer/apple_pie_roll.avif" },
+  { name: "Veggie Spring Roll", image: "/menu_images/appetizer/veggie_spring_roll.avif" }
+]
 
 // Main MenuBoard Component
 const MenuBoard: React.FC = () => {
@@ -100,6 +116,21 @@ const MenuBoard: React.FC = () => {
             <VStack spacing={3}>
               <Image width="300px" height="200px" src={entree.image} alt={entree.name} borderRadius="md" />
               <Text fontWeight="bold">{entree.name}</Text>
+            </VStack>
+          </GridItem>
+        ))}
+      </Grid>
+
+      {/* Appetizer Section */}
+      <Heading as="h2" size="lg" mb={4} textAlign="center">
+        Appetizers
+      </Heading>
+      <Grid templateColumns="repeat(auto-fit, minmax(300px, 1fr))" gap={6}>
+        {appetizers.map((appetizer, index) => (
+          <GridItem key={index} p={4} borderWidth="1px" borderRadius="md" boxShadow="md">
+            <VStack spacing={3}>
+              <Image width="300px" height="200px" src={appetizer.image} alt={appetizer.name} borderRadius="md" />
+              <Text fontWeight="bold">{appetizer.name}</Text>
             </VStack>
           </GridItem>
         ))}
