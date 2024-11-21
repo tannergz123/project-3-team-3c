a9d992accf04a825dbf3e93aac00f5af75efdc2c
apend10 <apendela10@gmail.com>
2024-11-21 16:40:37 -0600

    linked order-ingredients endpoint with ingredients order history to simplify the process of recording orders

--- backend/views/ingredients_order_history.py
+++ backend/views/ingredients_order_history.py
@@ -6,8 +6,8 @@ ingredients_order_history = Blueprint("ingredients_order_history", __name__)
 
 print("running ingredients_order_history.py")
 
-@ingredients_order_history.route("/add-record", methods=['POST'])
-def add_record():
+#helper method
+def add_record(ingredient:str, quantity_ordered:int):
     """
     Inserts a new record of an ingredient order.
 
@@ -16,25 +16,22 @@ def add_record():
     quantity_ordered : the number of units of the ingredient that was orderd
     """
     try:
-        # Extract parameters from query arguments
-        data = request.get_json()
-        ingredient = data.get('ingredient')
-        quantity_ordered = data.get('quantity_ordered', type=float)
         id = -1
 
         # Ensure acceptable parameters are provided 
         if ingredient is None or quantity_ordered is None:
-            return jsonify({ "status": "error", "message": "Both ingredient and quantity_ordered are required." }), 400
+            print("All of ingredient, quantity_ordered, and ingredients_order_id are required.")
         
         if quantity_ordered < 0 or quantity_ordered > 1000:
-            return jsonify({ "status": "error", "message": "quantity_ordered must be a positive value and be less than or equal 1000." }), 400
-        
+            print("quantity_ordered must be a positive value and be less than or equal 1000.")
+
         #get primary key and verify if the ingredient does actually exist
         cur = conn.cursor()
         cur.execute(f"SELECT ingredient_id from ingredients WHERE ingredient_name = '{ingredient}';")
 
         if cur.rowcount == 0:
-            return jsonify({ "status": "error", "message": "Given ingredient does not exist." }), 404
+            print(f"{ingredient} does not exist in the table.")
+            return False
         else:
             id = cur.fetchall()[0][0]
 
@@ -47,17 +44,12 @@ def add_record():
 
         if cur.rowcount == 0:
             return jsonify({ "status": "error", "message": "some error" }), 404
-        else:
-            print(f"{quantity_ordered} units of {ingredient} [{id}] successfully ordered on {current_datetime}")
 
         #close cursor
         conn.commit()
         cur.close()
 
-        return jsonify({"status": "successful"}), 200
+        return True
     except Exception as e:
         print("Error querying @ /ingredients_order_history/add_record ||", e)
-        return jsonify({
-            "status": "error",
-            "message": str(e)
-        }), 500
+        return False
